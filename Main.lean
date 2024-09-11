e363b1d926b126f27ec83e031b23ba784558be40
Marcus G Daniels <marcus@snoutfarm.com>
2024-09-10 17:30:25 -0700

    Remove some unused functions.

--- Main.lean
+++ Main.lean
@@ -9,38 +9,6 @@ open THF
 -- set_option pp.proofs true
 -- set_option trace.profiler true in
 
-def report0 (r : String × Except String MRS) :=
- match r.2 with
- | Except.ok b => IO.println $ List.length b.preds
- | Except.error e => IO.println (r.1, e)
-
-def report1 (r : String × Except String MRS) :=
- match r.2 with
- | Except.ok b => IO.println $
-   (b.preds.map (λ p : EP => p.predicate)).filter (λ s : String => s.endsWith "q")
- | Except.error e => IO.println e
-
-def reportMRS (mrs : MRS) : IO (Except String (Array Unit)) := do
-  let ret <- Utool.solveIt mrs
-  match ret with
-  | Except.ok expansions => 
-    let retArray <- expansions.mapM (fun mrs => IO.println $ f!"{mrs}")
-    return (Except.ok retArray)
-  | Except.error e2 => return (Except.error e2)
-
-def reportTHF (mrs : MRS) : IO (Except String (Array Unit)) := do
-  let ret <- Utool.solveIt mrs
-  match ret with
-  | Except.ok expansions => 
-    let retArray <- expansions.mapM (fun mrs => IO.println $ ((THF.MRS.format mrs) ++ "\n"))
-    return (Except.ok retArray)
-  | Except.error e2 => return (Except.error e2)
-
-def report2 (r : String × Except String MRS) : IO (Except String (Array Unit)) := do
-  match r.2 with
-  | Except.ok b => reportTHF b
-  | Except.error e => return (Except.error e)
-
 def solveAndFormat (mrs : MRS) : IO String := do
   let solveRet <- Utool.solveIt mrs
   match solveRet with
